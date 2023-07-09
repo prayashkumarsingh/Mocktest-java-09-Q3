56f7b157b5036c19e88d2ee0960607c7417136e3
prayashkumarsingh <88892535+prayashkumarsingh@users.noreply.github.com>
2023-07-09 20:02:43 +0530

    Add files via upload

---
+++ Q3.java
@@ -0,0 +1,42 @@
+class Person {
+    private String name;
+    private int age;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
+
+public class Q3 {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("Prayash Kumar Singh");
+        person.setAge(24);
+        person.setAddress("Cuttack,Odisha");
+
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+        System.out.println("Address: " + person.getAddress());
+    }
+}
